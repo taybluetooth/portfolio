db8d6bab1dc38d6e268b4b0e5762f3e3b979f7fe
taybluetooth <callumtaylorbusiness@gmail.com>
2022-10-05 01:55:57 +0100

    Added small loading screen.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,6 +19,8 @@ import {
   faTwitter,
 } from "@fortawesome/free-brands-svg-icons";
 
+import LogoSmall from "../components/LogoSmall";
+
 library.add(faLinkedin, faGithub, faInstagram, faTwitter);
 
 // import components
@@ -42,6 +44,22 @@ const Wrapper = styled.main`
   min-height: 100vh;
   max-width: 1400px;
   padding: 0 25px;
+`;
+
+const LoadLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+  justify-content: center;
+  align-items: center;
+`;
+
+const LoadWrapper = styled.main`
+  margin: 0 auto;
+  min-height: 100vh;
+  max-width: 1400px;
+  padding: 0 25px;
+  display: contents;
 `;
 
 const Modal = styled.div`
@@ -79,14 +97,19 @@ const SocialsList = styled.ul`
 `;
 
 const Social = styled.li`
-transition: 0.5s;
-&:hover {
-  transform: translate(0px, -5px);
-  box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.1);
-}
+  transition: 0.5s;
+  &:hover {
+    transform: translate(0px, -5px);
+    box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.1);
+  }
 `;
 
-const SocialsContainer = styled.div`orientation: left; bottom: 0; position: fixed; width: 40px;`
+const SocialsContainer = styled.div`
+  orientation: left;
+  bottom: 0;
+  position: fixed;
+  width: 40px;
+`;
 
 const SocialLink = styled.a`
   cursor: pointer;
@@ -104,7 +127,9 @@ const EmailContainer = styled.div`
   align-items: center;
   bottom: 0;
   right: 0;
-  orientation: right; position: fixed; width: 40px;
+  orientation: right;
+  position: fixed;
+  width: 40px;
   @media only screen and (max-width: 1024px) {
     display: none;
   }
@@ -136,67 +161,98 @@ const Email = styled.a`
   }
 `;
 
+const ImageContainer = styled.div`
+  width: 200px;
+  height: 200px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
 const IndexPage = () => {
   // set sidebar to be visible
   const [sidebarOpen, setSidebarOpen] = React.useState(false);
-  
+  const [loading, setLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2500);
+  }, []);
+
   return (
-    <Layout>
-      <Wrapper>
-        <Navbar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
-        <Modal
-          onMouseLeave={() => setSidebarOpen(false)}
-          style={sidebarOpen ? { width: "70%" } : {}}
-        >
-          <ResponsiveNavbar setSidebarOpen={setSidebarOpen}/>
-        </Modal>
-        <div style={sidebarOpen ? { filter: "blur(20px)" } : {}}>
-          <Header />
-          <About />
-          <Work />
-          <OtherWork />
-          <Contact />
-          <Footer />
-        </div>
-      </Wrapper>
-      <SocialsContainer><SocialsList>
-          <Social>
-            <SocialLink href="https://www.linkedin.com/in/callum-taylor-edinburgh/">
-              <FontAwesomeIcon
-                style={{ fontSize: "20px" }}
-                icon={["fab", "linkedin"]}
-              />
-            </SocialLink>
-          </Social>
-          <Social>
-            <SocialLink href="https://github.com/taybluetooth">
-              <FontAwesomeIcon
-                style={{ fontSize: "20px" }}
-                icon={["fab", "github"]}
-              />
-            </SocialLink>
-          </Social>
-          <Social>
-            <SocialLink href="https://www.instagram.com/taybluetooth/?hl=en-gb">
-              <FontAwesomeIcon
-                style={{ fontSize: "20px" }}
-                icon={["fab", "instagram"]}
-              />
-            </SocialLink>
-          </Social>
-          <Social>
-            <SocialLink href="https://twitter.com/tay_callum/">
-              <FontAwesomeIcon
-                style={{ fontSize: "20px" }}
-                icon={["fab", "twitter"]}
-              />
-            </SocialLink>
-          </Social>
-        </SocialsList></SocialsContainer>
-        <EmailContainer>
-          <Email href="mailto:callumtaylorbusiness@gmail.com?subject=Business Enquiry from Portfolio.">callumtaylorbusiness@gmail.com</Email>
-        </EmailContainer>
-    </Layout>
+    <>
+      {loading ? (
+        <LoadLayout>
+          <LoadWrapper>
+            <ImageContainer>
+              <LogoSmall />
+            </ImageContainer>
+          </LoadWrapper>
+        </LoadLayout>
+      ) : (
+        <Layout>
+          <Wrapper>
+            <Navbar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+            <Modal
+              onMouseLeave={() => setSidebarOpen(false)}
+              style={sidebarOpen ? { width: "70%" } : {}}
+            >
+              <ResponsiveNavbar setSidebarOpen={setSidebarOpen} />
+            </Modal>
+            <div style={sidebarOpen ? { filter: "blur(20px)" } : {}}>
+              <Header />
+              <About />
+              <Work />
+              <OtherWork />
+              <Contact />
+              <Footer />
+            </div>
+          </Wrapper>
+          <SocialsContainer>
+            <SocialsList>
+              <Social>
+                <SocialLink href="https://www.linkedin.com/in/callum-taylor-edinburgh/">
+                  <FontAwesomeIcon
+                    style={{ fontSize: "20px" }}
+                    icon={["fab", "linkedin"]}
+                  />
+                </SocialLink>
+              </Social>
+              <Social>
+                <SocialLink href="https://github.com/taybluetooth">
+                  <FontAwesomeIcon
+                    style={{ fontSize: "20px" }}
+                    icon={["fab", "github"]}
+                  />
+                </SocialLink>
+              </Social>
+              <Social>
+                <SocialLink href="https://www.instagram.com/taybluetooth/?hl=en-gb">
+                  <FontAwesomeIcon
+                    style={{ fontSize: "20px" }}
+                    icon={["fab", "instagram"]}
+                  />
+                </SocialLink>
+              </Social>
+              <Social>
+                <SocialLink href="https://twitter.com/tay_callum/">
+                  <FontAwesomeIcon
+                    style={{ fontSize: "20px" }}
+                    icon={["fab", "twitter"]}
+                  />
+                </SocialLink>
+              </Social>
+            </SocialsList>
+          </SocialsContainer>
+          <EmailContainer>
+            <Email href="mailto:callumtaylorbusiness@gmail.com?subject=Business Enquiry from Portfolio.">
+              callumtaylorbusiness@gmail.com
+            </Email>
+          </EmailContainer>
+        </Layout>
+      )}
+    </>
   );
 };
 
