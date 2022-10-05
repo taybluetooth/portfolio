d6568d669331be51c65372fe1c5e1ccc4570a4f9
taybluetooth <callumtaylorbusiness@gmail.com>
2022-10-05 10:29:08 +0100

    Updated to show socials at footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -44,10 +44,85 @@ const Description = styled.p`
   }
 `;
 
+const SocialsContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 30px;
+`;
+
+const SocialsList = styled.ul`
+  list-style-type: none;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 20px;
+  margin: 0;
+  padding: 0;
+  @media only screen and (min-width: 1024px) {
+    display: none;
+  }
+`;
+
+const Social = styled.li`
+  transition: 0.5s;
+  &:hover {
+    transform: translate(0px, -5px);
+    box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.1);
+  }
+`;
+
+
+const SocialLink = styled.a`
+  cursor: pointer;
+  text-decoration: none;
+  color: #c5c6c7bf;
+  transition: 0.5s;
+  &:hover {
+    color: #66fcf1;
+  }
+`;
+
 const Footer = () => {
   return (
     <Wrapper>
       <Container>
+      <SocialsContainer>
+            <SocialsList>
+              <Social>
+                <SocialLink href="https://www.linkedin.com/in/callum-taylor-edinburgh/">
+                  <FontAwesomeIcon
+                    style={{ fontSize: "20px" }}
+                    icon={["fab", "linkedin"]}
+                  />
+                </SocialLink>
+              </Social>
+              <Social>
+                <SocialLink href="https://github.com/taybluetooth">
+                  <FontAwesomeIcon
+                    style={{ fontSize: "20px" }}
+                    icon={["fab", "github"]}
+                  />
+                </SocialLink>
+              </Social>
+              <Social>
+                <SocialLink href="https://www.instagram.com/taybluetooth/?hl=en-gb">
+                  <FontAwesomeIcon
+                    style={{ fontSize: "20px" }}
+                    icon={["fab", "instagram"]}
+                  />
+                </SocialLink>
+              </Social>
+              <Social>
+                <SocialLink href="https://twitter.com/tay_callum/">
+                  <FontAwesomeIcon
+                    style={{ fontSize: "20px" }}
+                    icon={["fab", "twitter"]}
+                  />
+                </SocialLink>
+              </Social>
+            </SocialsList>
+            </SocialsContainer>
         <Description>Built by Callum Taylor.</Description>
         <Description>Design inspired by Brittany Chiang.</Description>
       </Container>
