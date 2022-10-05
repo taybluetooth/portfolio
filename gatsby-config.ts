771a2d0f5af11a68c01ab54cf3d339faab8f7a5c
taybluetooth <callumtaylorbusiness@gmail.com>
2022-10-05 13:06:00 +0100

    Added favicon.

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,13 +2,25 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    siteUrl: `https://www.callum-taylor.com`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `callum-taylor`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#0F1C2D`,
+        theme_color: `#0f1c2d`,
+        display: `minimal-ui`,
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
+      }
+    },
     {
       resolve: `gatsby-plugin-sharp`,
       options: {
@@ -24,7 +36,8 @@ const config: GatsbyConfig = {
           pngOptions: {},
           webpOptions: {},
           avifOptions: {},
-        }
+        },
+        icon: "./src/images/logo.svg"
       }
     },
     {
